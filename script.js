7e97fa34847106ad6f3db1251fa0cfc6784d7896
Vaibhavi Katmore <katmorevaibhavi@gmail.com>
2025-12-25 06:26:59 +0530

    Ref

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
- <script>
+ 
     // document.getElementById("btn").onclick = function () {
     //   // alert("It works!");
     // };
+    let countText = document.getElementById("countText");
     let count = 0;
     document.getElementById("btn").onclick = function () {
       count++;
       countText.innerText = "Clicked " + count + " times";
     };
-  </script>
+  
