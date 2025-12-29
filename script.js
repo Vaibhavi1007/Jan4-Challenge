730cccc9c1d9dc794cab3c2c2e564383eff85ee8
Vaibhavi Katmore <katmorevaibhavi@gmail.com>
2025-12-29 20:59:49 +0530

    Added input based counter with validation and encapsulated logic

--- script.js
+++ script.js
@@ -12,23 +12,24 @@ const counter = {
     },
 
     add(value){
-        if(value > 0){
-        for (let index = 0; index < value; index++) {
-            this.increment();
-        }
-    }
+        if (isNaN(value)) return "INVALID";
+        if (value <= 0) return "NON-POSITIVE";
+       
+        this.count += value;
+        this.updateText();
     },
 
     updateText() {
         const word = this.count === 1 ? "time" : "times";
-        if(countText){
-        countText.innerText = `Clicked ${this.count} ${word}`;
-        }
+        countText.innerText = `Clicked ${this.count} ${word}`;  
     }
 };
 
 let btn = document.getElementById("btn");
 let countText = document.getElementById("countText");
+let resetbtn = document.getElementById("resetbtn");
+let add = document.getElementById("add");
+const input = document.getElementById("input");
 
 btn.addEventListener("click", function () {
     counter.increment();
@@ -36,5 +37,16 @@ btn.addEventListener("click", function () {
 
 resetbtn.addEventListener("click" , function() {
     counter.reset();
+});
+
+add.addEventListener("click" , function(){   
+    const value = Number(input.value);
+    const result = counter.add(value);
+
+    if(result === "INVALID"){
+        alert("Please enter a valid number")
+    }else if(result === "NON-POSITIVE"){
+        "Please enter a number greater than 0"
+    }
 });
   
